03d36b765c3ee3ecbe4409aeec9ec34918efc6f3
Jakub Deniziak <jakub.deniziak@gmail.com>
2024-05-31 20:40:58 +0200

    Add footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="footer border-t fixed bottom-0 w-screen">
+            <p className="p-3">&copy; 2024 Jakub Deniziak</p>
+        </footer>
+    )
+}
+
+export default Footer
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Footer from "./components/Footer";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Vote counter</h1>
-    </main>
+    <div className="h-screen text-center">
+      <main>
+        <h1>Vote counter</h1>
+      </main>
+      <Footer />
+    </div>
   );
 }
