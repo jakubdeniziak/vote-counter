04ccd7af4d547c9aaeb453fe88785af8d0aca9d4
Jakub Deniziak <jakub.deniziak@gmail.com>
2024-05-31 20:46:43 +0200

    Add header

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header w-screen">
+            <h1 className="text-5xl p-5">Vote counter</h1>
+        </header>
+    )
+}
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div className="h-screen text-center">
+      <Header />
       <main>
-        <h1>Vote counter</h1>
       </main>
       <Footer />
     </div>
