5e90c05819493f9063106807be81ce61ffc2564e
Jakub Deniziak <jakub.deniziak@gmail.com>
2024-06-14 21:48:48 +0200

    Add icons for theme switching button

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,12 +1,28 @@
+import Image from "next/image"
+
+import Moon from "@/public/images/moon.svg"
+import Sun from "@/public/images/sun.svg"
+
+
 const Header = ({ toggleTheme, currentTheme }) => {
     return (
         <header className="header relative border-b border-accent">
             <h1 className="text-5xl p-5">Vote Counter</h1>
             <button 
                 onClick={toggleTheme}
-                className="absolute bottom-3 right-3 bg-accent text-secondary px-4 py-2 rounded"
+                className="absolute bottom-4 right-5 w-10"
             >
-                {currentTheme === "dark" ? "Light" : "Dark"}
+                {currentTheme === "dark" ?
+                    <Image
+                        src={Sun}
+                        alt="Turn on light mode"
+                    />
+                    :
+                    <Image
+                        src={Moon}
+                        alt="Turn on dark mode"
+                    />
+                }
             </button>
         </header>
     );
