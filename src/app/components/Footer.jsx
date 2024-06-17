e101fee9523dbab8188d9900bc1202fd4299aa1f
Jakub Deniziak <jakub.deniziak@gmail.com>
2024-06-17 23:15:50 +0200

    Fix footer background

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = ({theme}) => {
     return (
-        <footer className={`footer border-t border-accent fixed bottom-0 w-screen bg-${theme === "dark" ? "primary" : "secondary"}`}>
+        <footer className={`footer border-t border-accent fixed bottom-0 w-screen ${theme === "dark" ? "bg-primary" : "bg-secondary"}`}>
             <p className="p-3">&copy; 2024 Jakub Deniziak</p>
         </footer>
     )
