0c6872cfac4c1ff48be1607d6d4a3c4d728ab376
Jakub Deniziak <jakub.deniziak@gmail.com>
2024-06-19 11:26:16 +0200

    Center text in footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className={"footer border-t border-accent fixed bottom-0 w-screen"}>
+        <footer className={"footer border-t border-accent fixed bottom-0 w-screen text-center"}>
             <p className="p-3">&copy; 2024 Jakub Deniziak</p>
         </footer>
     )
