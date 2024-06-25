bacf83e822326ceb89983f3bbd63d1bd2d39a6d6
Jakub Deniziak <jakub.deniziak@gmail.com>
2024-06-25 22:59:35 +0200

    Fix indentation

--- src/app/components/setup/candidates/KnownCandidates.tsx
+++ src/app/components/setup/candidates/KnownCandidates.tsx
@@ -23,38 +23,38 @@ const KnownCandidates = ({candidates, setCandidates}: { candidates: Array<any>, 
 
     return (
         <div className="mb-4">
-                <input
-                    type="text"
-                    value={newCandidateName}
-                    onChange={handleNewCandidateChange}
-                    placeholder="New candidate name"
-                    className="border p-2 mr-2 text-primary"
-                />
-                <button
-                    onClick={addNewCandidate}
-                    className="bg-accent text-secondary px-4 py-2 rounded"
-                >
-                    Add
-                </button>
-                <div className="mt-5">
-                    {candidates.map((candidate: any, index: any) => (
-                        <div key={index} className="flex flex-row justify-center mb-2">
-                            <input
-                                type="text"
-                                value={candidate.name}
-                                readOnly
-                                className="border p-2 mr-2 text-primary"
-                            />
-                            <button
-                                onClick={() => removeCandidate(index)}
-                                className="bg-accent2 text-secondary px-4 py-2 rounded"
-                            >
-                                Remove
-                            </button>
-                        </div>
-                    ))}
-                </div>
+            <input
+                type="text"
+                value={newCandidateName}
+                onChange={handleNewCandidateChange}
+                placeholder="New candidate name"
+                className="border p-2 mr-2 text-primary"
+            />
+            <button
+                onClick={addNewCandidate}
+                className="bg-accent text-secondary px-4 py-2 rounded"
+            >
+                Add
+            </button>
+            <div className="mt-5">
+                {candidates.map((candidate: any, index: any) => (
+                    <div key={index} className="flex flex-row justify-center mb-2">
+                        <input
+                            type="text"
+                            value={candidate.name}
+                            readOnly
+                            className="border p-2 mr-2 text-primary"
+                        />
+                        <button
+                            onClick={() => removeCandidate(index)}
+                            className="bg-accent2 text-secondary px-4 py-2 rounded"
+                        >
+                            Remove
+                        </button>
+                    </div>
+                ))}
             </div>
+        </div>
     )
 }
 
