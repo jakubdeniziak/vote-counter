74b4c70c55e993d556aeb34fb427f4ecc8f87c30
Jakub Deniziak <jakub.deniziak@gmail.com>
2024-06-25 23:01:06 +0200

    Add placeholder for nominations

--- src/app/components/setup/candidates/CandidatesSetup.tsx
+++ src/app/components/setup/candidates/CandidatesSetup.tsx
@@ -1,11 +1,13 @@
 import React, { Dispatch } from 'react';
 import KnownCandidates from './KnownCandidates';
+import Nominations from './Nominations';
 
 const CandidatesSetup = ({candidates, setCandidates}: { candidates: Array<any>, setCandidates: Dispatch<Array<any>> }) => {
 
     return (
         <section className="mt-8">
             <h2 className="text-2xl font-semibold mb-3">Candidates</h2>
+            <Nominations/>
             <KnownCandidates
                 candidates={candidates}
                 setCandidates={setCandidates}
---
+++ src/app/components/setup/candidates/Nominations.tsx
@@ -0,0 +1,10 @@
+const Nominations = () => {
+
+    return (
+        <div className="mb-4">
+            <p>Nominations</p>
+        </div>
+    )
+}
+
+export default Nominations;
