3c94f0a9ef463b129376b1d02a3e0c274ef9f7bb
Jakub Deniziak <jakub.deniziak@gmail.com>
2024-06-25 23:13:18 +0200

    Add choosing between candidate input modes

--- src/app/components/setup/candidates/CandidatesSetup.tsx
+++ src/app/components/setup/candidates/CandidatesSetup.tsx
@@ -1,17 +1,44 @@
-import React, { Dispatch } from 'react';
+import React, { Dispatch, useState } from 'react';
+
 import KnownCandidates from './KnownCandidates';
 import Nominations from './Nominations';
 
 const CandidatesSetup = ({candidates, setCandidates}: { candidates: Array<any>, setCandidates: Dispatch<Array<any>> }) => {
 
+    const [candidateAdditionMode, setCandidateAdditionMode] = useState("")
+
+    const handleChange = (e: any) => {
+        setCandidateAdditionMode(e.target.value);
+    };
+
     return (
         <section className="mt-8">
             <h2 className="text-2xl font-semibold mb-3">Candidates</h2>
-            <Nominations/>
-            <KnownCandidates
-                candidates={candidates}
-                setCandidates={setCandidates}
-            />
+            <div className="my-6">
+                <button
+                    onClick={handleChange}
+                    value={"nominations"}
+                    className={`bg-accent text-secondary p-2 rounded mr-8 ${candidateAdditionMode === "nominations" ? "border border-accent2" : ""}`}
+                >
+                    Nominations
+                </button>
+                <button
+                    onClick={handleChange}
+                    value={"known"}
+                    className={`bg-accent text-secondary p-2 rounded mr-8 ${candidateAdditionMode === "known" ? "border border-accent2" : ""}`}
+                >
+                    Known
+                </button>
+            </div>
+            { candidateAdditionMode === "nominations" && (
+                <Nominations/>
+            )}
+            { candidateAdditionMode === "known" && (
+                <KnownCandidates
+                    candidates={candidates}
+                    setCandidates={setCandidates}
+                />
+            )}
         </section>
     );
 };
